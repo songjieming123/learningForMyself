8967c4edf67c4285ca5891deec1392bb2755c5b5
“songjieming123” <1569579546@qq.com>
2023-11-06 22:42:15 +0800

    第一次提交

---
+++ src/main/java/com/jieming/learnformyself/LearnformyselfApplication.java
@@ -0,0 +1,13 @@
+package com.jieming.learnformyself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnformyselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnformyselfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jieming/learnformyself/LearnformyselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jieming.learnformyself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnformyselfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
